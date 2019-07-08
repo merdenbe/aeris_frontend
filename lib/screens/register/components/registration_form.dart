4bf6bf18a493e20fd97b0a91b33c514a10167680
Michael Erdenberger <merdenbe@nd.edu>
2019-07-07 21:51:34 -0700

    still need to validate the password and the major

--- lib/screens/register/components/registration_form.dart
+++ lib/screens/register/components/registration_form.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:aris_frontend/screens/register/components/grad_year_dropdown.dart';
-import 'package:aris_frontend/screens/register/components/password_textfield.dart';
-import 'package:aris_frontend/screens/register/components/select_major.dart';
-import 'package:aris_frontend/screens/register/components/standard_text_field.dart';
+
+import 'package:validators/validators.dart';
+import 'package:validators/sanitizers.dart';
+
+import 'package:autocomplete_textfield/autocomplete_textfield.dart';
 
 class RegistrationForm extends StatefulWidget {
   @override
@@ -14,6 +15,61 @@ class RegistrationForm extends StatefulWidget {
 class RegistrationFormState extends State<RegistrationForm> {
 
   final _formKey = GlobalKey<FormState>();
+
+  // Show and hide password
+  bool _isHidden = true;
+  void _toggleVisibility(){
+    setState(() {
+      _isHidden = !_isHidden;
+    });
+  }
+
+
+  // Select major
+  List<String> added = [];
+
+  String currentText = "";
+
+  GlobalKey<AutoCompleteTextFieldState<String>> key = GlobalKey();
+
+  List<String> suggestions = [
+    "Computer Science",
+    "Computer Engineering",
+    "Mechanical Engineering",
+    "Chemical Engineering",
+    "Aerospace Engineering",
+    "Civil Engineering",
+    "Electrical Engineering"
+  ];
+
+  // Graduation year dropdown
+  List _grad_years = ['2020', '2021', '2022', '2023'];
+  List<DropdownMenuItem<String>> _dropDownMenuItems;
+  String _currentGradYear;
+
+  @override
+  void initState() {
+    _dropDownMenuItems = getDropDownMenuItems();
+    _currentGradYear = _dropDownMenuItems[0].value;
+    super.initState();
+  }
+
+  void changedDropDownItem(String selectedGradYear) {
+    setState(() {
+      _currentGradYear = selectedGradYear;
+    });
+  }
+
+  List<DropdownMenuItem<String>> getDropDownMenuItems() {
+    List<DropdownMenuItem<String>> items = List();
+    for (String grad_year in _grad_years) {
+      items.add(DropdownMenuItem(
+          value: grad_year,
+          child: Text(grad_year)
+      ));
+    }
+    return items;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,18 +84,147 @@ class RegistrationFormState extends State<RegistrationForm> {
               'Sign Up',
               style: new TextStyle(color: Colors.blue, fontSize: 25.0)
             ),
-            Padding(padding: EdgeInsets.only(top: 45.0)),
-            StandardTextField(title: 'First Name'),
+            Padding(padding: EdgeInsets.only(top: 40.0)),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Enter First Name',
+                fillColor: Colors.white,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(25.0),
+                  borderSide: BorderSide(),
+                ),
+              ),
+              validator: (firstName) {
+                // Sanitize the input
+                firstName = trim(firstName);
+
+                // Validate input
+                if (firstName.length > 50) {
+                  return 'First name must be under 50 characters.';
+                }
+                if (firstName.isEmpty) {
+                  return 'Enter some text';
+                }
+
+                firstName = '${firstName[0].toUpperCase()}${firstName.substring(1)}';
+
+                return null;
+              },
+            ),
             Padding(padding: EdgeInsets.only(top: 15.0)),
-            StandardTextField(title: 'Last Name'),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Enter Last Name',
+                fillColor: Colors.white,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(25.0),
+                  borderSide: BorderSide(),
+                ),
+              ),
+              validator: (lastName) {
+                // Sanitize the input
+                lastName = trim(lastName);
+
+                // Validate input
+                if (lastName.length > 50) {
+                  return 'First name must be under 50 characters.';
+                }
+                if (lastName.isEmpty) {
+                  return 'Enter some text';
+                }
+
+                lastName = '${lastName[0].toUpperCase()}${lastName.substring(1)}';
+              },
+            ),
             Padding(padding: EdgeInsets.only(top: 15.0)),
-            StandardTextField(title: 'Email'),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Enter Email',
+                fillColor: Colors.white,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(25.0),
+                  borderSide: BorderSide(),
+                ),
+              ),
+              validator: (email) {
+                // Sanitization
+                email = trim(email);
+
+                // Validation
+                if (!isEmail(email)) {
+                  return "Enter a valid email.";
+                }
+                if (email.substring(email.length - 6) != "nd.edu") {
+                  return "Must use a Notre Dame email.";
+                }
+                if (email.isEmpty) {
+                  return 'Enter some text';
+                }
+
+                return null;
+              },
+            ),
             Padding(padding: EdgeInsets.only(top: 15.0)),
-            PasswordTextField(),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Enter Password',
+                fillColor: Colors.white,
+                suffixIcon: IconButton(
+                    onPressed: _toggleVisibility,
+                    icon: _isHidden ? Icon(Icons.visibility_off) : Icon(Icons.visibility),
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(25.0),
+                  borderSide: BorderSide(),
+                ),
+              ),
+              obscureText: _isHidden,
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Enter some text';
+                }
+                return null;
+              },
+            ),
             Padding(padding: EdgeInsets.only(top: 15.0)),
-            SelectMajorDropdown(),
+            SimpleAutoCompleteTextField(
+              key: key,
+              decoration: InputDecoration(
+                labelText: 'Enter Major',
+                fillColor: Colors.white,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(25.0),
+                  borderSide: BorderSide(),
+                ),
+              ),
+              controller: TextEditingController(text: ""),
+              suggestions: suggestions,
+              textChanged: (text) => currentText = text,
+              clearOnSubmit: false,
+              textSubmitted: (text) => setState(() {
+                if (text != "") {
+                  added.add(text);
+                }
+              })
+            ),
             Padding(padding: EdgeInsets.only(top: 15.0)),
-            GradYearDropDown(),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    'Graduation Year: ',
+                    style: TextStyle(color: Colors.blue, fontSize: 20.0)
+                  ),
+                  DropdownButton(
+                      value: _currentGradYear,
+                      items: _dropDownMenuItems,
+                      onChanged: changedDropDownItem,
+                  ),
+                ]
+              ),
+            ),
             Padding(padding: EdgeInsets.only(top: 60.0)),
             Align(
               alignment: Alignment.bottomRight,
