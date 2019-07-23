5d5d8169e092131b489c12dfc983a3988533fde6
Michael Erdenberger <merdenbe@nd.edu>
2019-07-22 18:22:48 -0700

    The submit button works now

--- lib/screens/faq/screen.dart
+++ lib/screens/faq/screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
-
+import 'package:validators/sanitizers.dart';
 import 'package:aris_frontend/services/getQuestions.dart';
+import 'package:aris_frontend/services/poseQuestion.dart';
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class FAQScreen extends StatefulWidget{
   @override
@@ -13,6 +16,10 @@ class FAQScreenState extends State<FAQScreen>{
 
   List<Map<String, String>> questions = [];
 
+  TextEditingController questionController = TextEditingController();
+
+  String myToken = '';
+
   loadQuestions() {
     getQuestions().then((List<Map<String, String>> questions) {
       setState(() {
@@ -21,9 +28,84 @@ class FAQScreenState extends State<FAQScreen>{
     });
   }
 
+  getToken() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      myToken = prefs.getString('token');
+    });
+  }
+
   void initState() {
     super.initState();
     loadQuestions();
+    getToken();
+  }
+
+  _displayDialog(BuildContext context) async {
+    return showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('New Question'),
+          content: TextFormField(
+            maxLines: 5,
+            decoration: InputDecoration(
+              hintText: 'Enter question here...',
+              fillColor: Colors.white,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(25.0),
+                borderSide: BorderSide(),
+              ),
+            ),
+            controller: questionController,
+            keyboardAppearance: Brightness.light,
+            validator: (feedback) {
+              // Sanitize the input
+              feedback = trim(feedback);
+
+              // Validate input
+              if (feedback.isEmpty) {
+                return 'Enter some text';
+              }
+              if (feedback.length > 512) {
+                return 'Feedback must be under 512 characters.';
+              }
+            },
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Cancel', style: TextStyle(color: Colors.red)),
+              onPressed: () {
+                questionController.clear();
+                Navigator.of(context).pop();
+              },
+            ),
+            FlatButton(
+              child: Text('Submit', style: TextStyle(color: Colors.blue)),
+              onPressed: () {
+                // Validate input
+                String question = trim(questionController.text);
+                if (question.isEmpty || question.length > 512) {
+                  questionController.clear();
+                  Navigator.of(context).pop();
+                }
+
+                // Submit question
+                var body = json.encode({
+                  'question': question,
+                  'user_email': "merdenbe@nd.edu"
+                });
+                poseQuestion("https://aris-backend-staging.herokuapp.com/questions", body, myToken).then((int statusCode) {
+                  print(statusCode);
+                  Navigator.of(context).pop();
+                });
+              },
+            )
+          ],
+        );
+      }
+    );
   }
 
   @override
@@ -50,7 +132,7 @@ class FAQScreenState extends State<FAQScreen>{
         backgroundColor: Colors.blue,
         icon: Icon(Icons.add),
         label: Text('Ask New Question'),
-        onPressed: () {},
+        onPressed: () => _displayDialog(context),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       bottomNavigationBar: BottomAppBar(
